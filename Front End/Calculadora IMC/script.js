6e2668406768c34b95d1178ec19245709b7c59bc
GcM <111703592+gcm505@users.noreply.github.com>
2025-12-05 09:00:47 -0300

    indice sobre oque os valores significam

--- Front End/Calculadora IMC/script.js
+++ Front End/Calculadora IMC/script.js
@@ -3,11 +3,22 @@ function calcularIMC(){
  //Entrada de dados
     let peso = document.getElementById("peso").value;
     let altura = document.getElementById("altura").value;
-
+    let indice = "";
 //Processamento
     let imc = peso / (altura * altura);
-
+    if (imc < 18.4){
+        indice = "Abaixo do peso";
+    } else if (imc >= 18.5 && imc <= 24.9){
+        indice = "Normal";
+    } else if (imc >= 25 && imc <= 29.9){
+        indice = "Sobrepeso";
+    } else if (imc >= 30 && imc <= 39.9){
+        indice = "Obesidade";
+    } else {
+        indice = "Obesidade Severa";
+    }
 //Saida
-    document.getElementById("resultado").textContent = "IMC: " + imc;
+    document.getElementById("resultado").textContent = "IMC: " + imc.toFixed(2);
+    document.getElementById("indice").textContent = "Indice: " + indice;
 
 }

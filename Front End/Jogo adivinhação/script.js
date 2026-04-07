7763b777352528123c7166316ed2b10a0acbd2d1
GcM <111703592+gcm505@users.noreply.github.com>
2026-04-07 16:14:13 -0300

    mudança no JS e inicio do CSS
    80% JS feito e 30% do CSS

--- Front End/Jogo adivinhação/script.js
+++ Front End/Jogo adivinhação/script.js
@@ -1,22 +1,29 @@
+// Gerar um numero de 1 a 100 
+// Math.random = 0 - 0.999...  
+// Math.random()*100 = 0 - 99.999...
+// Math.floor(Math.random() * 100) = 0 - 99
+// Math.floor(Math.random() * 100) + 1 = 1 - 100
+
+const numeroAleatorio = Math.floor(Math.random() * 100) + 1; 
+let tentativas = 0;
+
 function checkGuess() {
-    const guessInput = document.getElementById("guessInput");
-    const feedback = document.getElementById("feedback");
-    const guess = parseInt(guessInput.value);
-    const randomNumber = Math.floor(Math.random() * 100) + 1;
+    let chute = parseInt(document.getElementById('chute').value);
+    tentativas++;
 
-    if (isNaN(guess)) {
-        feedback.textContent = "Por favor, insira um número válido.";
-        return;
+    if(tentativas == 5){
+        alert("infelizmente voce não adivinhou o numero "+ numeroAleatorio + " em 5 tentativas");
+        location.reload();
+    } // alert para teste de design, pode ser removido depois.
+
+    if(chute == numeroAleatorio){
+        document.getElementById('feedback').innerHTML = 'Parabéns! Você acertou o número ' + numeroAleatorio + ' em ' + tentativas + ' tentativas.';  
+    } else if (chute < numeroAleatorio) {
+        document.getElementById('feedback').innerHTML = 'O número é maior!';
+        document.getElementById('tentativas').innerHTML = tentativas;
+    } else {
+        document.getElementById('feedback').innerHTML = 'O número é menor!';
+        document.getElementById('tentativas').innerHTML = tentativas;
     }
-
-    while (guess !== randomNumber) {
-        if (guess > randomNumber) { 
-            feedback.textContent = "Seu palpite é maior que o número correto. Tente novamente.";} 
-        else {
-            feedback.textContent = "Seu palpite é menor que o número correto.";}
-    }
-
-    if (guess === randomNumber) {
-        feedback.textContent = "Parabéns! Você acertou o número!"; }
-        guessInput.disabled = true;
 }
+

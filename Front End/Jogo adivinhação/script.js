6bae66139a39d25d1af942bfe9ad99f080d56d66
GcM <111703592+gcm505@users.noreply.github.com>
2026-04-13 16:31:36 -0300

    Add files via upload

--- Front End/Jogo adivinhação/script.js
+++ Front End/Jogo adivinhação/script.js
@@ -5,25 +5,31 @@
 // Math.floor(Math.random() * 100) + 1 = 1 - 100
 
 const numeroAleatorio = Math.floor(Math.random() * 100) + 1; 
-let tentativas = 0;
+let tentativas = 10;
 
 function checkGuess() {
     let chute = parseInt(document.getElementById('chute').value);
-    tentativas++;
+    
 
-    if(tentativas == 5){
-        alert("infelizmente voce não adivinhou o numero "+ numeroAleatorio + " em 5 tentativas");
-        location.reload();
-    } // alert para teste de design, pode ser removido depois.
+    if(isNaN(chute) ||chute < 1 || chute >100){
+        alert("Por favor, insira um número entre 1 e 100.");
+        return;
+    }
+
+    tentativas--;
 
     if(chute == numeroAleatorio){
-        document.getElementById('feedback').innerHTML = 'Parabéns! Você acertou o número ' + numeroAleatorio + ' em ' + tentativas + ' tentativas.';  
+        document.getElementById('feedback').innerHTML = 'Parabéns! Você acertou o número ' + numeroAleatorio + ' em ' + (10 - tentativas) + ' tentativas.';  
+    } else if(tentativas == 0){
+        alert("Você perdeu! O número secreto era "+ numeroAleatorio); // alert para teste de design, pode ser removido depois. 
+        location.reload();
     } else if (chute < numeroAleatorio) {
         document.getElementById('feedback').innerHTML = 'O número é maior!';
-        document.getElementById('tentativas').innerHTML = tentativas;
+        document.getElementById('tentativas').innerHTML = 'Tentativas restantes: ' + tentativas;
     } else {
         document.getElementById('feedback').innerHTML = 'O número é menor!';
-        document.getElementById('tentativas').innerHTML = tentativas;
+        document.getElementById('tentativas').innerHTML = 'Tentativas restantes: ' + tentativas;
     }
-}
 
+    
+}
